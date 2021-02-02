85ac6caf489f5fd73a2ffb3eda321220d0fdfd88
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2021-02-02 14:31:30 +0530

    Image Optimzation

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="relative bg-white overflow-hidden">
+      <div className="relative bg-white overflow-hidden h-screen">
         <div className="max-w-7xl mx-auto">
           <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
             <svg
@@ -106,6 +106,8 @@ export default function Home() {
                   <div>
                     <img
                       className="h-8 w-auto"
+                      width="20"
+                      height="20"
                       src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                       alt=""
                     />
